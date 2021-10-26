93ef23fe92bbf8bfa09ec87fb8c44382b7c43b4f
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2021-10-26 19:27:59 -0400

    CSS Box

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
 
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
+
 import Saturn from '../assets/saturn.jpg';
 import SaturnNormal from '../assets/saturnNormal.jpg';
 import Earth from '../assets/earth.jpg';
@@ -26,13 +28,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 1000);
 
 camera.zoom = 79;
-camera.position.x = 3.583
-camera.position.y = -0.2
-camera.position.z = 29
-
-camera.rotation.x = 0
-camera.rotation.y = 0.146
-camera.rotation.z = 0
+// camera.position.setY(-199);
+// camera.position.x = 3.583;
+// camera.position.y = 100;
+// camera.position.z = 29;
+// camera.rotation.x = 0;
+// camera.rotation.y = 0;
+// camera.rotation.z = 0;
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -44,6 +46,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
 renderer.render(scene, camera);
+
 
 const geometry = new THREE.TorusGeometry(10, 1, 16, 100);
 // const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true});
@@ -65,17 +68,6 @@ scene.add(ambientLight);
 const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.minPolarAngle = controls.maxPolarAngle / 2;
-controls.maxPolarAngle = controls.minPolarAngle;
-controls.minAzimuthAngle = 0.146;
-controls.maxAzimuthAngle = Math.PI / 2 + 0.146;
-controls.enablePan = false;
-controls.maxZoom = 100;
-controls.minZoom = 60;
-
-controls.reset();
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -163,8 +155,27 @@ function moveCamera() {
 
 let obj;
 
+const renderer2 = new CSS3DRenderer();
+renderer2.setSize( window.innerWidth, window.innerHeight );
+renderer2.domElement.style.position = 'absolute';
+renderer2.domElement.style.top = 0;
+document.body.appendChild( renderer2.domElement );
+const scene2 = new THREE.Scene();
+
+const controls = new OrbitControls(camera, renderer2.domElement);
+controls.minPolarAngle = controls.maxPolarAngle / 2;
+controls.maxPolarAngle = controls.minPolarAngle;
+controls.minAzimuthAngle = 0;
+controls.maxAzimuthAngle = Math.PI / 2;
+controls.enablePan = false;
+controls.maxZoom = 100;
+controls.minZoom = 60;
+controls.reset();
+
 function animate() {
   requestAnimationFrame(animate);
+
+  // console.log(camera.rotation);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
@@ -181,22 +192,74 @@ function animate() {
   controls.update();
 
   renderer.render(scene, camera);
+  renderer2.render(scene2, camera);
 }
 
+// Create the 3D object for Subscribe
+
+const element = document.createElement( 'div' );
+element.className = 'element';
+element.innerHTML = '<h1>Subscribe</h1>';
+element.style.width = '10px';
+element.style.height = '10px';
+
+createPlane(
+
+					4, 3,
+					'chocolate',
+					new THREE.Vector3(  0, -3, 0 ),
+					// new THREE.Euler( 180 * THREE.MathUtils.DEG2RAD,  180 * THREE.MathUtils.DEG2RAD, 180 * THREE.MathUtils.DEG2RAD )
+          new THREE.Euler(0, 0, 0)
+				);
+
+
+function createPlane(width, height, cssColor, pos, rot) {
+    const element = document.createElement("div");
+    element.className = "element";
+    element.style.width = width + "px";
+    element.style.height = height + "px";
+    element.style.opacity = 0.75;
+    element.style.background = cssColor;
+
+    const text = document.createElement("div");
+    text.className = "label";
+    text.innerHTML = "TEST";
+    element.appendChild(text);
+
+    const object = new CSS3DObject(element);
+    object.position.copy(pos);
+    object.rotation.copy(rot);
+    scene2.add(object);
+
+    // const geometry = new THREE.PlaneGeometry(width, height);
+    // const mesh = new THREE.Mesh(geometry, material);
+    // mesh.position.copy(object.position);
+    // mesh.rotation.copy(object.rotation);
+    // scene.add(mesh);
+    const geometry = new THREE.BoxGeometry(width, height, height);
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.copy(object.position);
+    mesh.position.x += .1
+    mesh.position.z -= height / 2;
+    mesh.rotation.copy(object.rotation);
+    scene.add(mesh);
+}
+
+/// END
 
 const mtlLoader = new MTLLoader();
 
 function onLoad(object) {
   scene.add(object);
   obj = object;
+  obj.position.y += 0.5;
   obj.scale.x = .05;
   obj.scale.y = .05;
   obj.scale.z = .05;
 
   obj.rotation.x = 0;
-  obj.rotation.y -= 40;
+  obj.rotation.y -= 40.1;
   obj.rotation.z = 0;
-
 
   animate();
 }
