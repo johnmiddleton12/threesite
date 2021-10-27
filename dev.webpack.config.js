f6696b13a749cc57e0e5fd2ded80fb4f0ed19205
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2021-10-26 23:44:51 -0400

    Mail Form Initial

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',                // only for development
     entry: './src/index.js',
+    devtool: 'inline-source-map',       // only for development
+    devServer: {                        // only for development
+        static: './dist',
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
--- src/index.js
+++ src/index.js
@@ -1,33 +1,48 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader';
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
+import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader";
 
-import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer';
+import {
+    CSS3DRenderer,
+    CSS3DObject,
+} from "three/examples/jsm/renderers/CSS3DRenderer";
 
-import Saturn from '../assets/saturn.jpg';
-import SaturnNormal from '../assets/saturnNormal.jpg';
-import Earth from '../assets/earth.jpg';
-import EarthNormal from '../assets/earthNormal.jpg';
+import Saturn from "../assets/saturn.jpg";
+import SaturnNormal from "../assets/saturnNormal.jpg";
+import Earth from "../assets/earth.jpg";
+import EarthNormal from "../assets/earthNormal.jpg";
 
-import Landscape from '../assets/landscape.jpg';
-import Pyramid from '../assets/pyramid.jpg';
-import Space from '../assets/space.jpg';
+import Landscape from "../assets/landscape.jpg";
+import Pyramid from "../assets/pyramid.jpg";
+import Space from "../assets/space.jpg";
 
-import AnamorphicText from '../assets/anamorphicText2.obj';
-import AnamorphicTextMtl from '../assets/anamorphicText2.mtl';
+import AnamorphicText from "../assets/anamorphicText2.obj";
+import AnamorphicTextMtl from "../assets/anamorphicText2.mtl";
 
+// TODO: Implement window resize function from Three JS example
+// TODO: Clean code
+// TODO: Round edges of box
+// TODO: QR Code on side of box
+// TODO: Prettify
 
 const scene = new THREE.Scene();
 
 // const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 1000);
+const camera = new THREE.OrthographicCamera(
+    window.innerWidth / -2,
+    window.innerWidth / 2,
+    window.innerHeight / 2,
+    window.innerHeight / -2,
+    1,
+    1000
+);
 
-camera.zoom = 79;
+camera.zoom = 4;
 // camera.position.setY(-199);
 // camera.position.x = 3.583;
 // camera.position.y = 100;
@@ -37,20 +52,19 @@ camera.zoom = 79;
 // camera.rotation.z = 0;
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
+    canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-camera.position.setZ(30);
+camera.position.setZ(200);
 
 renderer.render(scene, camera);
 
-
 const geometry = new THREE.TorusGeometry(10, 1, 16, 100);
 // const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true});
-const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 
 const torus = new THREE.Mesh(geometry, material);
 
@@ -60,7 +74,7 @@ const pointLight = new THREE.PointLight(0xffffff);
 
 pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+const ambientLight = new THREE.AmbientLight(0xffffff);
 
 // scene.add(pointLight);
 scene.add(ambientLight);
@@ -70,14 +84,16 @@ const gridHelper = new THREE.GridHelper(200, 50);
 // scene.add(lightHelper, gridHelper);
 
 function addStar() {
-  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
-  const material = new THREE.MeshStandardMaterial({ color: 0XFFFFFF });
-  const star = new THREE.Mesh(geometry, material);
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
 
-  const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
 
-  star.position.set(x, y, z);
-  scene.add(star);
+    star.position.set(x, y, z);
+    scene.add(star);
 }
 
 Array(200).fill().forEach(addStar);
@@ -88,8 +104,8 @@ scene.background = winterTexture;
 const pyramidTexture = new THREE.TextureLoader().load(Pyramid);
 
 const pyramid = new THREE.Mesh(
-  new THREE.ConeBufferGeometry(5, 9, 4, 2),
-  new THREE.MeshBasicMaterial({ map: pyramidTexture })
+    new THREE.ConeBufferGeometry(5, 9, 4, 2),
+    new THREE.MeshBasicMaterial({ map: pyramidTexture })
 );
 
 // scene.add(pyramid);
@@ -99,22 +115,22 @@ const saturnTexture = new THREE.TextureLoader().load(Saturn);
 const normalSaturn = new THREE.TextureLoader().load(SaturnNormal);
 
 const saturn = new THREE.Mesh(
-  new THREE.SphereGeometry(4, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: saturnTexture,
-    normalMap: normalSaturn
-  })
+    new THREE.SphereGeometry(4, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: saturnTexture,
+        normalMap: normalSaturn,
+    })
 );
 
 const earthTexture = new THREE.TextureLoader().load(Earth);
 const normalEarth = new THREE.TextureLoader().load(EarthNormal);
 
 const earth = new THREE.Mesh(
-  new THREE.SphereGeometry(4, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: earthTexture,
-    normalMap: normalEarth
-  })
+    new THREE.SphereGeometry(4, 32, 32),
+    new THREE.MeshStandardMaterial({
+        map: earthTexture,
+        normalMap: normalEarth,
+    })
 );
 
 scene.add(saturn, earth);
@@ -131,24 +147,20 @@ torus.position.setZ(-25);
 torus.position.setX(10);
 
 function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+    // console.log(t);
+    saturn.rotation.x += 0.05;
+    saturn.rotation.y += 0.075;
+    saturn.rotation.z += 0.05;
 
-  const t = document.body.getBoundingClientRect().top;
-  // console.log(t);
-  saturn.rotation.x += 0.05;
-  saturn.rotation.y += 0.075;
-  saturn.rotation.z += 0.05;
+    earth.rotation.x += 0.03;
+    earth.rotation.y += 0.05;
+    earth.rotation.z += 0.07;
 
-
-  earth.rotation.x += 0.03;
-  earth.rotation.y += 0.05;
-  earth.rotation.z += 0.07;
-
-  camera.position.z = t * -0.0159;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * 0.01;
-
-
-};
+    camera.position.z = t * -0.0159;
+    camera.position.x = t * -0.0002;
+    camera.position.y = t * 0.01;
+}
 
 // document.body.onscroll = moveCamera;
 // moveCamera();
@@ -156,10 +168,10 @@ function moveCamera() {
 let obj;
 
 const renderer2 = new CSS3DRenderer();
-renderer2.setSize( window.innerWidth, window.innerHeight );
-renderer2.domElement.style.position = 'absolute';
+renderer2.setSize(window.innerWidth, window.innerHeight);
+renderer2.domElement.style.position = "absolute";
 renderer2.domElement.style.top = 0;
-document.body.appendChild( renderer2.domElement );
+document.body.appendChild(renderer2.domElement);
 const scene2 = new THREE.Scene();
 
 const controls = new OrbitControls(camera, renderer2.domElement);
@@ -169,51 +181,77 @@ controls.minAzimuthAngle = 0;
 controls.maxAzimuthAngle = Math.PI / 2;
 controls.enablePan = false;
 controls.maxZoom = 100;
-controls.minZoom = 60;
+// controls.minZoom = 60;
 controls.reset();
 
 function animate() {
-  requestAnimationFrame(animate);
+    requestAnimationFrame(animate);
 
-  // console.log(camera.rotation);
+    // console.log(camera.rotation);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.005;
+    torus.rotation.z += 0.01;
 
-  // obj.rotation.x += 0.01;
+    // obj.rotation.x += 0.01;
 
-  earth.rotation.x += 0.02;
+    earth.rotation.x += 0.02;
 
-  saturn.rotation.x += 0.0001;
-  saturn.rotation.y += 0.0003;
-  saturn.rotation.z += 0.0002;
+    saturn.rotation.x += 0.0001;
+    saturn.rotation.y += 0.0003;
+    saturn.rotation.z += 0.0002;
 
-  controls.update();
+    controls.update();
 
-  renderer.render(scene, camera);
-  renderer2.render(scene2, camera);
+    renderer.render(scene, camera);
+    renderer2.render(scene2, camera);
 }
 
-// Create the 3D object for Subscribe
+let formHTML = `
+<form action="https://gmail.us5.list-manage.com/subscribe/post?u=8b8fb485ceda61427cdb2dcc3&amp;id=95daa11532" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	<h2>Subscribe</h2>
+<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+<div class="mc-field-group">
+	<label for="mce-EMAIL">Email Address  <span class="asterisk" style="color: red">*</span>
+</label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" autocomplete="off">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>  
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+</form>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+`;
 
-const element = document.createElement( 'div' );
-element.className = 'element';
-element.innerHTML = '<h1>Subscribe</h1>';
-element.style.width = '10px';
-element.style.height = '10px';
+createBox(
+    310,
+    70,
+    "chocolate",
+    new THREE.Vector3(0, -90, 0),
+    // new THREE.Euler( 180 * THREE.MathUtils.DEG2RAD,  180 * THREE.MathUtils.DEG2RAD, 180 * THREE.MathUtils.DEG2RAD )
+    new THREE.Euler(0, 0, 0)
+);
 
-createPlane(
+var checkExist = setInterval(function () {
+    if (document.getElementById("mc-embedded-subscribe") != null) {
+        document
+            .getElementById("mc-embedded-subscribe")
+            .addEventListener("mouseover", function () {
+                controls.enabled = false;
+            });
+        document
+            .getElementById("mc-embedded-subscribe")
+            .addEventListener("mouseout", function () {
+                controls.enabled = true;
+            });
+        clearInterval(checkExist);
+    }
+}, 100); // check every 100ms
 
-					4, 3,
-					'chocolate',
-					new THREE.Vector3(  0, -3, 0 ),
-					// new THREE.Euler( 180 * THREE.MathUtils.DEG2RAD,  180 * THREE.MathUtils.DEG2RAD, 180 * THREE.MathUtils.DEG2RAD )
-          new THREE.Euler(0, 0, 0)
-				);
-
-
-function createPlane(width, height, cssColor, pos, rot) {
+function createBox(width, height, cssColor, pos, rot) {
     const element = document.createElement("div");
     element.className = "element";
     element.style.width = width + "px";
@@ -221,26 +259,25 @@ function createPlane(width, height, cssColor, pos, rot) {
     element.style.opacity = 0.75;
     element.style.background = cssColor;
 
+    element.style.justifyContent = "center";
+
     const text = document.createElement("div");
-    text.className = "label";
-    text.innerHTML = "TEST";
+    text.id = "mc_embed_signup";
+    text.innerHTML = formHTML;
+
     element.appendChild(text);
 
     const object = new CSS3DObject(element);
     object.position.copy(pos);
+    object.position.z += height / 2;
     object.rotation.copy(rot);
     scene2.add(object);
 
-    // const geometry = new THREE.PlaneGeometry(width, height);
-    // const mesh = new THREE.Mesh(geometry, material);
-    // mesh.position.copy(object.position);
-    // mesh.rotation.copy(object.rotation);
-    // scene.add(mesh);
     const geometry = new THREE.BoxGeometry(width, height, height);
     const mesh = new THREE.Mesh(geometry, material);
     mesh.position.copy(object.position);
-    mesh.position.x += .1
     mesh.position.z -= height / 2;
+    mesh.position.x += 0.5;
     mesh.rotation.copy(object.rotation);
     scene.add(mesh);
 }
@@ -250,28 +287,30 @@ function createPlane(width, height, cssColor, pos, rot) {
 const mtlLoader = new MTLLoader();
 
 function onLoad(object) {
-  scene.add(object);
-  obj = object;
-  obj.position.y += 0.5;
-  obj.scale.x = .05;
-  obj.scale.y = .05;
-  obj.scale.z = .05;
+    scene.add(object);
+    obj = object;
+    obj.position.y += 0.5;
+    obj.scale.x = 1;
+    obj.scale.y = 1;
+    obj.scale.z = 1;
 
-  obj.rotation.x = 0;
-  obj.rotation.y -= 40.1;
-  obj.rotation.z = 0;
+    obj.rotation.x = 0;
+    obj.rotation.y -= 40.1;
+    obj.rotation.z = 0;
 
-  animate();
+    animate();
 }
 
 mtlLoader.load(AnamorphicTextMtl, function (materials) {
-  materials.preload();
-  new OBJLoader().setMaterials(materials).load(AnamorphicText, onLoad,
-    function (xhr) {
-      console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    },
-    function (error) {
-      console.log("an error happened");
-    }
-  );
-})
+    materials.preload();
+    new OBJLoader().setMaterials(materials).load(
+        AnamorphicText,
+        onLoad,
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+        },
+        function (error) {
+            console.log("an error happened");
+        }
+    );
+});
